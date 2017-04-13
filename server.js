298784f8ae19cc93b00213ec8818e4d1a8faa319
Brian Bleakley <brianbleakley@gmail.com>
2017-04-13 14:43:10 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var htmlToJson = require("html-to-json")
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send("<a href='roll'>Roll!</a>")
+})
+
+app.get('/roll', function (req, res) {
+  htmlToJson.request('http://www.recursionpharma.com/team.html', {
+    'data': {
+      $container: '#741471631897133620-gallery',
+      'team': function ($t) {
+        htmlToJson.parse($t.html(), {
+          'employees': ['.galleryInnerImageHolder', function ($galleryInnerImageHolder) {
+            	return $galleryInnerImageHolder.find('a').attr('href');
+          }]
+        }, function (err, result) {
+          var employeeCount = result.employees.length;
+          console.log(employeeCount + ' employees');
+          console.log(result);
+          var randomEmployee = result.employees[Math.floor(Math.random()*employeeCount)];
+          console.log('randomly selected ' + randomEmployee)
+          res.redirect('http://www.recursionpharma.com' + randomEmployee);
+        });
+      }
+    }
+  });
+})
+
+app.listen(3000)
