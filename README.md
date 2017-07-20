c9a508763e38821337b9b1b6c7c65a2e83982998
Brian Bleakley <brianbleakley@gmail.com>
2017-07-20 11:54:05 -0600

    replaced html parse with bamboohr api

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Employee Picker
-Rolls a random Recursionaut. Parses the html of recursionpharma.com/team.html.
+A slackbot that replies to direct messages with random employees (pulled from BambooHR's API).
 
 ## Start
 Copy the API key from [here](https://recursion.slack.com/apps/A0F7YS25R-bots), add to secrets.slackApiToken.
--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-var htmlToJson = require("html-to-json")
+var request = require("request");
 var SlackBot = require('slackbots');
 var secrets = require("./secrets.json");
 
-var recursionautList = [];
+var employeeList = [];
+
+var bambooHrSubdomain = secrets.bambooHrSubdomain;
+var bambooHrApiKey = secrets.bambooHrApiKey;
 
 
 function updateEmployeeList() {
-  htmlToJson.request('http://www.recursionpharma.com/team.html', {
-    'data': {
-      $container: '#153152873426670938-gallery',
-      'team': function ($t) {
-        htmlToJson.parse($t.html(), {
-          'employees': ['.galleryInnerImageHolder', function ($galleryInnerImageHolder) {
-            	return $galleryInnerImageHolder.find('a').attr('href');
-          }]
-        }, function (err, result) {
-          recursionautList = result.employees;
-          boardOfDirectorsElement = recursionautList.indexOf('/uploads/9/4/2/1/942150/slide61_orig.gif');
-          recursionautList = recursionautList.slice(0, boardOfDirectorsElement);
-          recursionautList = recursionautList.filter(x => {
-            return !x.startsWith('/uploads');
-          });
-          console.log(recursionautList);
-          console.log(recursionautList.length);
-        });
-      }
-    }
+
+  var options = { method: 'GET',
+    url: `https://${bambooHrApiKey}:x@api.bamboohr.com/api/gateway.php/${bambooHrSubdomain}/v1/employees/directory`,
+    headers:
+     { accept: 'application/json' } };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    employeeList = JSON.parse(body).employees;
+    console.log(employeeList);
   });
+
   setTimeout(updateEmployeeList, 1000*60*60*24);
 }
 
@@ -44,10 +37,9 @@ var params = {
 bot.on('message', function(data) {
     if(data.type == 'message' && data.user != 'U4ZED7AN7') {
       bot.getUserById(data.user).then(function(u){
-        var randomEmployee = recursionautList[Math.floor(Math.random()*recursionautList.length)];
-        var url = 'http://www.recursionpharma.com' + randomEmployee;
-        console.log(u.name + ' requested employee, received ' + url);
-        bot.postMessageToUser(u.name, 'hi ' + u.name + ' your random employee is ' + url, params);
+        var randomEmployee = employeeList[Math.floor(Math.random()*employeeList.length)];
+        console.log(u.name + ' requested employee, received ' + randomEmployee.displayName);
+        bot.postMessageToUser(u.name, `hi ${u.name} your random employee is ${randomEmployee.displayName}\n${randomEmployee.photoUrl}`, params);
       })
     }
 });
